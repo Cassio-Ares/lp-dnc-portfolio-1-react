0bdfa55bafe668c8065b526a0bbb4be2c97bb922
Cassio-Ares <cassiotadeu.ads@gmail.com>
2023-08-21 09:40:27 -0300

    use state e useEffect

--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -1,19 +1,37 @@
 import ButtonB from './ButtonB'
 import styles from './Cards.module.css'
+import { useState } from 'react'
 
 
 
 function Cards({text, img, name}){
+     
+    const [info , setInfo]= useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.cards}> 
+        <div className={styles.cards} onMouseLeave={InfoOff}> 
             <figure>
-               <img src={img}/>
+               <img onMouseEnter={ InfoOn}  src={img}/>
             </figure>
-            <section>
+
+            {
+                info === true  &&(
+                    <section>
                 <h3>Projeto {name}</h3>
                 <p><strong>Tecnologias:</strong>{text}</p>
             <ButtonB text='Acesse o repositório'/>
             </section>
+                )
+            }
+            
             
         </div>
     )
