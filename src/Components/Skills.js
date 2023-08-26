0c1f5c011319b547bb65c278899def4876e3d2de
Cassio-Ares <cassiotadeu.ads@gmail.com>
2023-08-26 19:29:30 -0300

    Versão para Produção2

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,16 +5,32 @@ import js from '../Image/skills/js.svg'
 import react from '../Image/skills/react.svg'
 import tp from '../Image/skills/tp.svg'
 import CardInform from '../Elements/CardInform'
+import {useState} from 'react'
 
 
 function Skills(){
+    const [infor, setInfor]= useState(false)
+
+    function info(){
+        setInfor(true)
+    }
+
+    function infoNot(){
+        setInfor(false)
+    }
+
     return(
         <div id="Skills" className={styles.skills}>
             <h1>Habilidades</h1>
             <p>Conheça um pouco das minhas principais habilidades e conhecimentos.</p>
-            <figure>
-                <img src={html}/> 
+            <figure onMouseOut={infoNot}>
+                <img src={html}  onMouseEnter={info}/> 
+                { infor === true && (
+                    <CardInform title="HTML"  text=" é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores."/>
+                 )
+                 }
                 <img src={css}/>
+                 
                 <img src={js}/>
                 <img src={react}/>
                 <img src={tp}/>
--- src/Elements/CardInform.js
+++ src/Elements/CardInform.js
@@ -3,7 +3,7 @@ import styles from './CardInform.module.css'
 function CardInform({text, title}){
     return(
         <div className={styles.cardInform}> 
-          <p><span>HTML</span> é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores.</p>
+          <p><span>{title}</span>{text}</p>
            
         </div>
     )
